b4179450774026c3f1b12299af1cbbfd90e4321f
itsVentie <ventieravelle@gmail.com>
2026-08-25 20:21:21 +0200

    feat(network): add Relay client and DCUtR to MarrowBehaviour

--- crates/network/src/behaviour.rs
+++ crates/network/src/behaviour.rs
@@ -1,8 +1,10 @@
 use crate::codec::MarrowCodec;
 use libp2p::autonat;
+use libp2p::dcutr;
 use libp2p::identify;
 use libp2p::kad::{store::MemoryStore, Behaviour as Kademlia};
 use libp2p::ping;
+use libp2p::relay;
 use libp2p::request_response::Behaviour as RequestResponse;
 use libp2p::swarm::NetworkBehaviour;
 
@@ -12,5 +14,7 @@ pub struct MarrowBehaviour {
     pub identify: identify::Behaviour,
     pub ping: ping::Behaviour,
     pub autonat: autonat::Behaviour,
+    pub relay_client: relay::client::Behaviour,
+    pub dcutr: dcutr::Behaviour,
     pub req_resp: RequestResponse<MarrowCodec>,
 }
