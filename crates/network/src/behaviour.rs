3b370dda9b04349fc917e5afe7eddc53cb848474
itsVentie <ventieravelle@gmail.com>
2026-08-25 15:43:47 +0200

    feat(network): define composite MarrowBehaviour structure

---
+++ crates/network/src/behaviour.rs
@@ -0,0 +1,16 @@
+use crate::codec::{MarrowCodec, MarrowProtocol};
+use libp2p::autonat;
+use libp2p::identify;
+use libp2p::kad::{store::MemoryStore, Behaviour as Kademlia};
+use libp2p::ping;
+use libp2p::request_response::json::Behaviour as RequestResponse;
+use libp2p::swarm::NetworkBehaviour;
+
+#[derive(NetworkBehaviour)]
+pub struct MarrowBehaviour {
+    pub kademlia: Kademlia<MemoryStore>,
+    pub identify: identify::Behaviour,
+    pub ping: ping::Behaviour,
+    pub autonat: autonat::Behaviour,
+    pub req_resp: RequestResponse<MarrowProtocol, MarrowCodec>,
+}
